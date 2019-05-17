5243b780bc7f217485c15f74bd9707d01756e183
Devon Mitchell <devonamitchell10@gmail.com>
2019-05-17 10:30:11 -0500

    rendered original list to DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { connect } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pizzaArray: [],
+    }
+  }
+
+  componentDidMount() {
+    this.getOriginalPizzaList();
+  }
+
+  getOriginalPizzaList() {
+    axios.get('/api/pizza')
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          pizzaArray: res.data
+        });
+        console.log(this.state)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+
   render() {
+    const pizzaHTML = this.state.pizzaArray.map((pizza, index) => {
+      return (
+        <div key={index}>
+          <img src={pizza.image_path} alt={pizza.description}/>
+          <p>{pizza.name}</p>
+          <p>{pizza.description}</p>
+          <p>{pizza.price}</p>
+        </div>
+      )
+    })
     return (
       <div className="App">
         <header className="App-header">
@@ -12,6 +53,7 @@ class App extends Component {
         <br/>
         <img src="images/pizza_photo.png"/>
         <p>Pizza is great.</p>
+        {pizzaHTML}
       </div>
     );
   }
